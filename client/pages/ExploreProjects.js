a1126764e3aebf2d649a6dec2310e2d2c8cc8cdb
spjy <spjy@hawaii.edu>
2019-11-10 12:10:23 -1000

    feat: implement explore projects page

--- client/pages/ExploreProjects.js
+++ client/pages/ExploreProjects.js
@@ -1,16 +1,89 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Button, Layout, Text } from 'react-native-ui-kitten';
+import { StyleSheet, View } from 'react-native';
+import { Button, Layout, Text, Icon } from 'react-native-ui-kitten';
 import { Actions } from 'react-native-router-flux';
+import { Card } from '../components/Card';
+
+const categories = [
+  {
+    name: 'Clean Energy',
+    icon: 'charging-outline',
+    description: 'Sustainability is the name of the game.'
+  },
+  {
+    name: 'Local Food',
+    icon: 'car-outline',
+    description: 'Sustainability is the name of the game.'
+  },
+  {
+    name: 'Resources',
+    icon: 'globe-2-outline',
+    description: 'Sustainability is the name of the game.'
+  },
+  {
+    name: 'Waste',
+    icon: 'trash-outline',
+    description: 'Sustainability is the name of the game.'
+  },
+  {
+    name: 'Communities',
+    icon: 'bulb-outline',
+    description: 'Sustainability is the name of the game.'
+  },
+  {
+    name: 'Education',
+    icon: 'people-outline',
+    description: 'Sustainability is the name of the game.'
+  }
+]
 
 export const ExploreProjects = () => (
   <Layout style={styles.container}>
-    <Text style={styles.text} category='h4'>Categories</Text>
-    <Button onPress={Actions.Project}>BUTTON</Button>
+    <Text style={{ paddingTop: 15 }} category='h4'>Categories</Text>
+    <Text style={{ paddingBottom: 10 }} category="p1" appearance="hint">
+      Select a category to get involved.
+    </Text>
+    <Layout style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+      {
+        categories.map(({
+          name,
+          description,
+          icon
+        }) => (
+          <Card
+            key={name}
+            name={name}
+            description={description}
+            icon={icon}
+          />
+        ))
+      }
+    </Layout>
   </Layout>
 );
+
+function elevationShadowStyle(elevation) {
+  return {
+    elevation,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 0.5 * elevation },
+    shadowOpacity: 0.3,
+    shadowRadius: 0.8 * elevation
+  };
+}
 
 const styles = StyleSheet.create({
   container: { flex: 1, alignItems: 'center' },
   text: { marginVertical: 16 },
+  shadow: {
+    // Sweet! Android shadow!
+    ...elevationShadowStyle(5)
+  },
+  box: {
+    borderRadius: 8,
+    backgroundColor: 'white',
+    padding: 15,
+    margin: 10,
+    width: '45%'
+  }
 });
