55c7ffe1b0e4397fe405e6b22fe7cee29bea812b
spjy <spjy@hawaii.edu>
2019-11-10 14:09:59 -1000

    feat: update migration names

--- server/models/categories.js
+++ server/models/categories.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const category = sequelize.define('Category', {
     name: DataTypes.STRING,
-    icon: DataTypes.STRING
+    icon: DataTypes.STRING,
+    description: DataTypes.STRING
   }, {});
   category.associate = function(models) {
     category.belongsToMany(models.Project, {through: 'ProjectCategories', foreignKey: 'projectID', as: 'origin'})
