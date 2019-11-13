d302a0dad03d30cb699b09249d63131086ff6cb8
mirabela <EvanYoung@FloWaters-Air.lan1>
2019-11-13 09:46:15 -1000

    nope

--- client/App.js
+++ client/App.js
@@ -24,51 +24,64 @@ import { MyImpactCategory } from './pages/MyImpactCategory';
 import { Dashboard } from './pages/Dashboard';
 import { DashboardCategory } from './pages/DashboardCategory';
 
-const App = () => (
+import * as Font from 'expo-font';
+
+
+export default class App extends React.Component {
+  componentDidMount() {
+    Font.loadAsync({
+      'icomoon': require('../client/assets/fonts/icomoon.ttf'),
+    });
+    this.setState({ fontLoaded: true });
+  }
+  
+  render() {
+    return (
+      
   <React.Fragment>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider mapping={mapping} theme={lightTheme}>
-      <Router>
-        <Stack key="root"  titleStyle={{ alignSelf: 'center' }}>
-          <Scene hideNavBar panHandlers={null}>
-            <Tabs key="tabbar" routeName="tabbar" backToInitial >
-              <Stack initial key="Explore" title="Explore"
-              inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={ExploreTabIcon}>
-                <Scene initial key="ExploreCategories" title="Explore Categories" component={ExploreCategories}/>
-                <Scene lazy back key="ExploreCategoryProjects" title="Explore Projects" component={ExploreCategoryProjects} hideDrawerButton/>
-                <Scene lazy back key="ProjectPage" title="Project Details" component={ProjectPage} hideDrawerButton/>
-              </Stack>
+  <IconRegistry icons={EvaIconsPack} />
+  <ApplicationProvider mapping={mapping} theme={lightTheme}>
+    <Router>
+      <Stack key="root"  titleStyle={{ alignSelf: 'center' }}>
+        <Scene hideNavBar panHandlers={null}>
+          <Tabs key="tabbar" routeName="tabbar" backToInitial >
+            <Stack initial key="Explore" title="Explore"
+            inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={ExploreTabIcon}>
+              <Scene initial key="ExploreCategories" title="Explore Categories" component={ExploreCategories}/>
+              <Scene lazy back key="ExploreCategoryProjects" title="Explore Projects" component={ExploreCategoryProjects} hideDrawerButton/>
+              <Scene lazy back key="ProjectPage" title="Project Details" component={ProjectPage} hideDrawerButton/>
+            </Stack>
 
-              <Stack key="Projects" title="Projects" 
-              inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={ProjectsTabIcon} hideDrawerButton>
-                <Scene initial key="MyProjects" title="My Projects" component={MyProjects}/>
-                <Scene lazy back key="ProjectPage" title="Project Details" component={ProjectPage}/>
-                <Scene lazy back key="EditProjectPage" title="Edit Project" component={EditProjectPage}/>
-                <Scene lazy back key="AddProjectResults" title="Add Results"  component={AddProjectResults}/>
-              </Stack>
+            <Stack key="Projects" title="Projects" 
+            inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={ProjectsTabIcon} hideDrawerButton>
+              <Scene initial key="MyProjects" title="My Projects" component={MyProjects}/>
+              <Scene lazy back key="ProjectPage" title="Project Details" component={ProjectPage}/>
+              <Scene lazy back key="EditProjectPage" title="Edit Project" component={EditProjectPage}/>
+              <Scene lazy back key="AddProjectResults" title="Add Results"  component={AddProjectResults}/>
+            </Stack>
 
-              <Stack key="Add" title="Add a Project" tabBarLabel= "Add"
-              inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={AddTabIcon} hideDrawerButton>
-                <Scene initial key="AddProject" title="Add a Project" component={AddProject}/>
-              </Stack>
-              
-              <Stack key="Impact" title="My Impact"
-              inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={ImpactTabIcon} hideDrawerButton>
-                <Scene initial key="MyImpact" title="My Impact" component={MyImpact}/>
-                <Scene lazy back key="MyImpactCategory" title="My Impact"  component={MyImpactCategory}/>
-              </Stack>
+            <Stack key="Add" title="Add a Project" tabBarLabel= "Add"
+            inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={AddTabIcon} hideDrawerButton>
+              <Scene initial key="AddProject" title="Add a Project" component={AddProject}/>
+            </Stack>
+            
+            <Stack key="Impact" title="My Impact"
+            inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={ImpactTabIcon} hideDrawerButton>
+              <Scene initial key="MyImpact" title="My Impact" component={MyImpact}/>
+              <Scene lazy back key="MyImpactCategory" title="My Impact"  component={MyImpactCategory}/>
+            </Stack>
 
-              <Stack key="Dash" title="Dashboard" 
-              inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={DashboardTabIcon} hideDrawerButton>
-                <Scene initial key="Dashboard" title="Dashboard" component={Dashboard}/>
-                <Scene lazy back key="DashboardCategory" title="Dashboard" component={DashboardCategory}/>
-              </Stack>
-            </Tabs>
-          </Scene>
-        </Stack>
-      </Router>
-    </ApplicationProvider>
-  </React.Fragment>
-);
-
-export default App;
+            <Stack key="Dash" title="Dashboard" 
+            inactiveBackgroundColor="#919191" activeBackgroundColor="#000000" icon={DashboardTabIcon} hideDrawerButton>
+              <Scene initial key="Dashboard" title="Dashboard" component={Dashboard}/>
+              <Scene lazy back key="DashboardCategory" title="Dashboard" component={DashboardCategory}/>
+            </Stack>
+          </Tabs>
+        </Scene>
+      </Stack>
+    </Router>
+  </ApplicationProvider>
+</React.Fragment>
+    );
+  }
+}
