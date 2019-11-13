fc435cba5ad1a275a87c165b2741278bacbc4abd
mirabela <EvanYoung@FloWaters-Air.lan1>
2019-11-12 21:39:38 -1000

    m

--- client/components/ProjectCard.js
+++ client/components/ProjectCard.js
@@ -6,12 +6,38 @@ import { Text, Icon } from 'react-native-ui-kitten';
 import { Actions } from 'react-native-router-flux';
 
 
+
+
+
 export class ProjectCard extends React.Component {
   
 
+  seeProject = () => Actions.ProjectPage({
+    project: "Waikiki Biking",
+    categories: [1,2,3],
+    date: "Thu, Nov. 28 11:00 - 16:00",
+    participants: 24,
+    summary: "Join us for a day of biking along the beach and reduce the amount of CO2 in the atmosphere by choosing an alternative mode of transportation. You also benefit from increased health and cardiovascular activity. There will be 40 bikes available! Meet us at the Statue!",
+    report: "A high turn out this time. We reserved about twenty bikes but needed sixty! During the time, we toured what Waikiki had to offer with its scenic Layouts, impeccable food and deceiving tourist traps.",
+    statistics: [
+      {
+        category: 'CLEAN ENERGY',
+        metric: 50,
+        unit: 'tons',
+        description: 'of CO2 offset'
+      },
+      {
+        category: 'LOCAL FOOD',
+        metric: 1000,
+        unit: 'USD',
+        description: 'into economy'
+      }
+    ]
+  })
+
   render() {
     return (
-    <TouchableOpacity activeOpacity={0.95} onPress={Actions.ProjectPage}>
+    <TouchableOpacity activeOpacity={0.95} onPress={this.seeProject}>
         <ImageBackground style={styles.container} source={{uri: `${this.props.projectImage}`}}>
         <View style={styles.overlay}>
             <View  style={styles.chipContainer}>
--- client/pages/ProjectPage.js
+++ client/pages/ProjectPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {  StyleSheet, ScrollView, Image, Dimensions  } from 'react-native';
-import { Button, Layout, Text, List } from 'react-native-ui-kitten';
-import { CategoryButton } from '../components/CategoryButton';
-import { Actions } from 'react-native-router-flux';
+import { StyleSheet, View, Image, ScrollView } from 'react-native';
+import {
+  Layout,
+  Icon,
+  TopNavigation,
+  TopNavigationAction,
+  Text,
+  Avatar
+} from 'react-native-ui-kitten';
+import { IndicatorStats } from '../components/ProjectPage/IndicatorStats';
+
 
 export class ProjectPage extends React.Component {
 
@@ -12,22 +19,123 @@ export class ProjectPage extends React.Component {
     return (
       <ScrollView style={styles.container} bounces={false} bouncesZoom={false} 
         alwaysBounceVertical={false} alwaysBounceHorizontal={false} {...this.props}>
-          <Image resizeMode={'contain'}style={styles.image} source={{uri: 'https://i.ibb.co/T8pZ4WQ/Project-Page.png'}}></Image>
-        </ScrollView>
+      <Image
+        style={{ width: '100%', height: 225 }}
+        source={{ uri: 'http://www.hawaiimagazine.com/sites/default/files/field/image/HiM1708-AY-Biki-Bikes-6377.jpg' }}
+      />
+      <Layout style={styles.outer} level="3">
+        <Layout style={styles.body}>
+          <ScrollView >
+            <Text category="h4">{this.props.project}</Text>
+            <Layout style={[styles.flexRow, styles.padding]}>
+              <Icon name='charging-outline' width={26} height={26} fill='#FF6961' />
+              <Icon name='car-outline' width={26} height={26} fill='#FF6961' />
+              <Icon name='globe-2-outline' width={26} height={26} fill='#ccc' />
+              <Icon name='trash-outline' width={26} height={26} fill='#ccc' />
+              <Icon name='bulb-outline' width={26} height={26} fill='#ccc' />
+              <Icon name='people-outline' width={26} height={26} fill='#ccc' />
+            </Layout>
+            <Layout style={{ paddingBottom: 10 }}>
+              <Layout style={styles.flexRow}>
+                <Layout>
+                  <Icon name='clock-outline' width={20} height={20} fill='#3366FF' />
+                </Layout>
+                <Layout style={{ paddingTop: 4, paddingLeft: 2 }}>
+                  <Text appearance="hint" category="c1">
+                    {this.props.date}
+                  </Text>
+                </Layout>
+              </Layout>
+              <Layout style={styles.flexRow}>
+                <Layout>
+                  <Icon name='person-outline' width={20} height={20} fill='#3366FF' />
+                </Layout>
+                <Layout style={{ paddingTop: 5, paddingLeft: 2 }}>
+                  <Text appearance="hint" category="c1">
+                    {this.props.participants} participants
+                  </Text>
+                </Layout>
+              </Layout>
+            </Layout>
+          </ScrollView>
+        </Layout>
+              <View>
+              <Text style={{marginTop:15}}>
+              {this.props.summary}
+            </Text>
+            <Text style={styles.title} category="h6">Project Report</Text>
+                <Text>
+                  {this.props.report}
+                </Text>
+              </View>
+              <View>
+                <Text style={styles.title}category="h6">Project Statistics</Text>
+                <View style={styles.flexRow}>
+                  {
+                    this.props.statistics.map(({
+                      category,
+                      metric,
+                      unit,
+                      description
+                    }) => (
+                      <IndicatorStats
+                        key={category}
+                        category={category}
+                        metric={metric}
+                        unit={unit}
+                        description={description}
+                      />
+                    ))
+                  }
+                </View>
+            </View>
+      </Layout>
+      </ScrollView>
     );
   }
 }
 
-const win = Dimensions.get('window');
+ProjectPage.propTypes = {
+  project: PropTypes.string.isRequired,
+  categories: PropTypes.arrayOf(PropTypes.number).isRequired,
+  date: PropTypes.string.isRequired,
+  participants: PropTypes.number.isRequired,
+  summary: PropTypes.string.isRequired,
+  report: PropTypes.string,
+  statistics: PropTypes.arrayOf(PropTypes.shape({
+    category: PropTypes.string,
+    metric: PropTypes.number,
+    unit: PropTypes.string,
+    description: PropTypes.string
+  }))
+};
 
+ProjectPage.defaultProps = {
+  report: '',
+  statistics: []
+};
 
 const styles = StyleSheet.create({
-  image: {
-    flex: 1,
-    width: win.width,
-    height: win.height,
+  container: { flex: 1, alignContent: 'center' },
+  outer: {
+    padding: 15,
   },
-  container: {
-    flex: 1,
-  }
+  body: {
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginTop: -40,
+  },
+  text: { marginVertical: 16 },
+  flexRow: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  flexCol: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  title: {
+    paddingTop: 10
+  },
 });
