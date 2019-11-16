91d30222bb2d6352bfde793ed5f44db1b16d8765
mirabela <EvanYoung@flowater.lan1>
2019-11-15 16:12:00 -1000

    m

--- client/pages/MyImpact.js
+++ client/pages/MyImpact.js
@@ -8,6 +8,27 @@ import { Actions } from 'react-native-router-flux';
 export class MyImpact extends React.Component {
 
 
+  totalprojects = 0;
+  cat1 = 0;
+  cat2 = 0;
+  cat3 = 0;
+  cat4 = 0;
+  cat5 = 0;
+  cat6 = 0;
+
+  
+  //filter all projects where people ==userid, host == userid
+  // PIE GRAPH 
+  //filter projects where categories includes catid
+  // length of that = cat1 
+  // add to totalprojects
+
+  //LINE GRAPH month history 3 months
+  // get project endsAt
+  // new Date from string
+  // check if  date.getMonth()  == 10, 11, 12, 1
+  // plot oct, nov, dec, jan
+
   render() {
     return (
       <ScrollView style={styles.container} bounces={false} bouncesZoom={false} 
